10c4e30100c884546f774ebe09ab74e84a40008c
Rob Murdock <rob.murdock@integral.io>
2020-09-08 09:21:50 -0400

    First step - synchronous test

--- src/Example.js
+++ src/Example.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export function Example(props) {
+export function Example() {
+    const [enabled, setIsEnabled] = useState(false)
+
     return <div>
-        Example
+        <button onClick={() => setIsEnabled(true)}>
+            Press me
+        </button>
+        <div>
+            {enabled ?
+                "Stop poking me"
+                : ""
+            }
+        </div>
     </div>
 }
--- src/Example.test.js
+++ src/Example.test.js
@@ -1,9 +1,13 @@
 import {render} from "@testing-library/react";
 import React from "react";
 import {Example} from "./Example";
+import userEvent from "@testing-library/user-event";
 
-test('renders example content', () => {
-    const {getByText} = render(<Example/>);
-    const linkElement = getByText(/Example/i);
-    expect(linkElement).toBeInTheDocument();
+test('clicking button will show text', () => {
+    const {queryAllByText, getByText, queryByText} = render(<Example/>);
+    const button = getByText(/Press me/i);
+    expect(queryAllByText(/Stop poking me/i)).toEqual([])
+    expect(button).toBeInTheDocument();
+    userEvent.click(button)
+    expect(queryByText(/Stop poking me/i)).toBeInTheDocument()
 });
