2e0227402fa440546e0c6edd09ecccfb0a1e7e16
Rob Murdock <rob.murdock@integral.io>
2020-09-08 13:08:23 -0400

    Wrapping all wait behavior inside of act, to avoid react warning,.

--- src/Example.test.js
+++ src/Example.test.js
@@ -4,19 +4,27 @@ import {Example} from "./Example";
 import userEvent from "@testing-library/user-event";
 import {waitFor} from "@testing-library/dom";
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 test('clicking button will show value from async service', async () => {
     const expectedText = "Stop poking me, please"
-    const textService = async () => expectedText
+    const textService = async () => {
+        await sleep(10)
+        return expectedText
+    }
 
     const {queryAllByText, getByText, queryByText} = render(<Example textService={textService}/>);
 
     const button = getByText(/Press me/i);
     expect(queryAllByText(expectedText)).toEqual([])
 
-    userEvent.click(button)
+    await act(async () => {
+        userEvent.click(button)
 
-    await waitFor(() =>
-        expect(queryByText(expectedText)).toBeInTheDocument()
-    )
-
+        await waitFor(() =>
+            expect(queryByText(expectedText)).toBeInTheDocument()
+        )
+    })
 });
