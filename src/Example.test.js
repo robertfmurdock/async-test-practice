8a9b6f73eeec90df74457b72e910a4e82beb3f5d
Rob Murdock <rob.murdock@integral.io>
2020-09-17 09:26:48 -0400

    Intentional not-quite-right version of the async test.

--- src/Example.test.js
+++ src/Example.test.js
@@ -1,13 +1,20 @@
-import {render} from "@testing-library/react";
+import {act, render} from "@testing-library/react";
 import React from "react";
 import {Example} from "./Example";
 import userEvent from "@testing-library/user-event";
 
-test('clicking button will show text', () => {
-    const {queryAllByText, getByText, queryByText} = render(<Example/>);
-    const button = getByText(/Press me/i);
-    expect(queryAllByText(/Stop poking me/i)).toEqual([])
-    expect(button).toBeInTheDocument();
-    userEvent.click(button)
-    expect(queryByText(/Stop poking me/i)).toBeInTheDocument()
+test('clicking button will show value from async service', async () => {
+    await act(async () => {
+        const expectedText = "Stop poking me, please"
+        const textService = async () => expectedText
+
+        const {queryAllByText, getByText, queryByText} = render(<Example textService={textService}/>);
+
+        const button = getByText(/Press me/i);
+        expect(queryAllByText(expectedText)).toEqual([])
+
+        userEvent.click(button)
+
+        expect(queryByText(expectedText)).toBeInTheDocument()
+    });
 });
